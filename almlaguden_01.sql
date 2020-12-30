c304efa04f50d9d5c6ff251a3274c0f5d4335d83
almlaguden21661453 <56673072+almlaguden21661453@users.noreply.github.com>
2020-12-30 13:48:39 +0300

    Add files via upload

---
+++ almlaguden_01.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 06 Kas 2020, 12:20:45
+-- Sunucu sürümü: 10.4.14-MariaDB
+-- PHP Sürümü: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `almlaguden_01`
+--
+CREATE DATABASE IF NOT EXISTS `almlaguden_01` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `almlaguden_01`;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kitap`
+--
+
+CREATE TABLE `kitap` (
+  `Kitapkayittarihi` time NOT NULL DEFAULT current_timestamp() COMMENT 'Kitap veri tabanına kaydedildiği zamanı gösterir.',
+  `kitapNo` int(255) NOT NULL COMMENT 'Kitaba ait kayıt numarası içerir.',
+  `ISBN` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'İlgili kitaba ait ISBN numarası bilgisi.',
+  `KitapAdi` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Kitabın adı.',
+  `KitabinBasimYeri` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'kitabın basım yeri bilgisi.',
+  `KitabinBasimyili` year(4) NOT NULL COMMENT 'Kitabın basım yılı bilgisi.',
+  `KitabinBskbilgisi` int(10) DEFAULT NULL COMMENT 'kitabın baskı bilgisi.',
+  `Kitabindili` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'kitabın dil bilgisi.',
+  `Hangiyöreyeait` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'İçeriğin yöre bilgisi.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `kitap`
+--
+
+INSERT INTO `kitap` (`Kitapkayittarihi`, `kitapNo`, `ISBN`, `KitapAdi`, `KitabinBasimYeri`, `KitabinBasimyili`, `KitabinBskbilgisi`, `Kitabindili`, `Hangiyöreyeait`) VALUES
+('22:09:52', 1, '9786054232253', '81 İl 81 Ünlü  81 Yemek', 'Ankara', 2009, NULL, 'Türkçe', 'Türkiye'),
+('22:10:12', 2, '9786051069470', 'Doğu Anadolu Yemekleri', 'İstanbul', 2015, NULL, 'Türkçe', 'Doğu Anadolu'),
+('22:10:31', 3, '9789755216348', 'Akdeniz Yemekleri : pratik mutfak rehberi', 'İstanbul', 2006, NULL, 'türkçe', 'Akdeniz'),
+('22:11:36', 4, '9789750821745', 'İstanbul Rumlarından Yemek Tarifleri', 'İstanbul', 2012, NULL, 'Türkçe', 'Marmara'),
+('22:11:36', 5, '9786051069487', 'Güneydoğu Anadolu Yemekleri', 'İstanbul', 2015, NULL, 'Türkçe', 'Güneydoğu Anadolu'),
+('22:11:36', 6, '9789751413918', 'Antakya ve Yemekleri', 'İstanbul ', 2010, NULL, 'Türkçe ', 'Akdeniz'),
+('22:11:36', 7, '9786056170935', 'Anadolu Yemekleri Ve Türk Mutfağı', 'İstanbul', 2011, 2, 'Türkçe', 'Türkiye'),
+('22:11:36', 8, '9789757375326', 'Dört mevsim Gaziantep yemekleri', 'Gaziantep ', 2003, NULL, 'Türkçe', 'Güneydoğu Anadolu'),
+('22:11:36', 9, '9786051069500', 'Soframda Anadolu Karadeniz Yemekleri', 'İstanbul ', 2015, NULL, 'türkçe', 'Karadeniz'),
+('22:11:36', 10, '9789755852669', 'ELAZIĞ / HARPUT YÖRESEL YEMEKLERİ', 'Elazığ', 2002, NULL, 'Türkçe', 'Doğu Anadolu ');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `yayinevi`
+--
+
+CREATE TABLE `yayinevi` (
+  `yayinevino` int(100) NOT NULL COMMENT 'yayınevi numarası yer almaktadır.',
+  `yayıneviadi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'yayınevi adı bilgisi yer almaktadır.',
+  `yayıneviadresi` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'yayınevi adres bilgisi yer almaktadır.',
+  `yayınevieposta` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'yayınevi e posta bilgisi yer almaktadır.',
+  `yayinevitelefon` int(11) DEFAULT NULL COMMENT 'yayınevi telefon bilgisi yer almaktadır.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `yayinevi`
+--
+
+INSERT INTO `yayinevi` (`yayinevino`, `yayıneviadi`, `yayıneviadresi`, `yayınevieposta`, `yayinevitelefon`) VALUES
+(1, 'Yapı Kredi Yayınları', 'İstiklal Caddesi No: 161 34433, Beyoğlu / İstanbul', 'iletisim@ykykultur.com.tr', 2123600063),
+(2, 'Remzi Kitabevi', 'Akmerkez E3 Blok Kat:14,\r\n34337 Etiler, İstanbul - Türkiye', 'post@remzi.com.tr', 2122822080),
+(3, 'Elazığ Kültür ve Yardımlaşma Derneği', 'Halk Cad. No:50 / 2-3 Üsküdar / İSTANBUL', 'info@elfed.org.tr', NULL),
+(4, 'Çiya Yayınları', 'Güneşlibahçe Sokak 38/2 Kadıköy İstanbul', 'yemekvekultur@gmail.com', NULL),
+(5, 'Elips Yayıncılık', 'Akpınar Mahallesi 826. Sokak No: 30 / 13 Dikmen / Çankaya / Ankara', 'elipskitap@elipskitap.com.tr', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `yazar`
+--
+
+CREATE TABLE `yazar` (
+  `yazarno` int(11) NOT NULL COMMENT 'Yazar ıd numarası.',
+  `yazaradi` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Yazar adı bilgisi yer almaktadır.',
+  `yazarsoyadi` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Yazar soyadı bilgisi yer almaktadır.',
+  `yazaradisoyadi` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Yazar adı soyadı bilgisi yer almaktadır.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `yazar`
+--
+
+INSERT INTO `yazar` (`yazarno`, `yazaradi`, `yazarsoyadi`, `yazaradisoyadi`) VALUES
+(1, 'Ahmet ', 'Bulut ', 'Ahmet Bulut '),
+(2, 'M. Faruk ', 'Bayrak', 'M. Faruk Bayrak'),
+(3, 'Özden Mermer', ' Özsabuncuoğlu', 'Özden Mermer Özsabuncuoğlu'),
+(4, 'Akile ', 'Ülkücan', 'Akile Ülkücan'),
+(5, 'Jale', 'Balcı', 'Jale Balcı'),
+(6, 'sula ', 'Bozis', 'Sula Bozis');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kitap`
+--
+ALTER TABLE `kitap`
+  ADD PRIMARY KEY (`kitapNo`);
+
+--
+-- Tablo için indeksler `yayinevi`
+--
+ALTER TABLE `yayinevi`
+  ADD PRIMARY KEY (`yayinevino`);
+
+--
+-- Tablo için indeksler `yazar`
+--
+ALTER TABLE `yazar`
+  ADD PRIMARY KEY (`yazarno`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kitap`
+--
+ALTER TABLE `kitap`
+  MODIFY `kitapNo` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Kitaba ait kayıt numarası içerir.', AUTO_INCREMENT=11;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `yayinevi`
+--
+ALTER TABLE `yayinevi`
+  MODIFY `yayinevino` int(100) NOT NULL AUTO_INCREMENT COMMENT 'yayınevi numarası yer almaktadır.', AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
